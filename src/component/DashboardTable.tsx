948f9c0944ff43619141f1388b0163a3ae986254
Arun V S @ 4i Apps <arun.s@4iapps.com>
2024-07-10 10:46:45 +0530

    finished dashboard page

---
+++ src/component/DashboardTable.tsx
@@ -0,0 +1,45 @@
+import { Column } from "react-table";
+import TableHOC from "./TableHOC";
+
+
+interface Datatype {
+    id: string,
+    quantity: number,
+    discount: number,
+    amount: number,
+    status: string;
+}
+
+const columns: Column<Datatype>[] = [{
+    Header: "Id",
+    accessor: "id",
+},
+{
+    Header: "Quantity",
+    accessor: "quantity",
+},
+{
+    Header: "Discount",
+    accessor: "discount",
+},
+{
+    Header: "Amount",
+    accessor: "amount",
+},
+{
+    Header: "Status",
+    accessor: "status",
+},
+];
+
+
+const DashboardTable = ({ data = [] }: { data: DataType[] }) => {
+    return TableHOC<DataType>(
+      columns,
+      data,
+      "transaction-box",
+      "Top Transaction"
+    )();
+  };
+  
+  export default DashboardTable;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,9 @@ import userImg from "../assets/userpic.png";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 
 import data from "../assets/data.json";
-
+import { BarChart, DoughnutChart } from "../component/Charts";
+import { BiMaleFemale } from "react-icons/bi";
+import Table from "../component/DashboardTable";
 const dashboard = () => {
   return (
     <div className="adminContainer">
@@ -37,21 +39,46 @@ const dashboard = () => {
           <div className="revenueChart">
             <h2>Revenue & Transaction</h2>
             {/* Graph here */}
+            <BarChart
+              data_2={[300, 144, 433, 655, 237, 755, 190]}
+              data_1={[200, 444, 343, 556, 778, 455, 990]}
+              title_1="Revenue"
+              title_2="Transaction"
+              bgColor_1="rgb(0,115,255)"
+              bgColor_2="rgba(53,162,235,0.8)"
+            />
           </div>
           <div className="dashboardCategories">
             <h2>Inventory</h2>
             <div>
               {
-                data.categories.map(i=>(
+                data.categories.map(i => (
                   <CategoryItem key={i.heading}
-                  color={`hsl(${i.value*4}, ${i.value}%, 50%)`}
-                  value={i.value}
-                  heading={i.heading} />
+                    color={`hsl(${i.value * 4}, ${i.value}%, 50%)`}
+                    value={i.value}
+                    heading={i.heading} />
                 ))
               }
             </div>
           </div>
         </section>
+        <section className="transaction-container">
+          <div className="gender-chart">
+            <h2>Gender Ratio</h2>
+            <DoughnutChart
+              labels={["Female", "Male"]}
+              data={[12, 19]}
+              backgroundColor={["hsl(340,82%,56%)", "rgba(53,162,235,0.8)"]}
+              cutout={90}
+            />
+            <p>
+              <BiMaleFemale />
+            </p>
+          </div>
+
+          <Table data={data.transaction} />
+        </section>
+
 
       </main>
 
@@ -76,25 +103,25 @@ const WidgetItem = ({ heading, value, percent, color, amount }: WidgetItemProps)
   <article className="widget">
     <div className="widgetInfo">
       <p>{heading}</p>
-      <h4>{amount ? `$${value}`: value}</h4>
+      <h4>{amount ? `$${value}` : value}</h4>
       {
         percent > 0 ?
           (
             <span className="green"><HiTrendingUp />+{percent}%{" "}</span>
-          ):(
+          ) : (
             <span className="red"><HiTrendingDown />{percent}%{" "}</span>
           )
       }
     </div>
 
     <div className="widgetCircle"
-    style={{
-      background:`conic-gradient(${color} ${(Math.abs(percent)/100)*360}deg, rgb(255,255,255) 0)`
-    }}>
+      style={{
+        background: `conic-gradient(${color} ${(Math.abs(percent) / 100) * 360}deg, rgb(255,255,255) 0)`
+      }}>
       <span style={{
         color,
       }}>{percent}%</span>
-     </div>
+    </div>
   </article>
 
 
@@ -104,18 +131,18 @@ interface CategoryItemProps {
   color: string;
 }
 
-const CategoryItem = ({color, value, heading}: CategoryItemProps) => (
-<div className="categoryItem">
-  <h5>{heading}</h5>
-  <div>
-    <div style={{
-      backgroundColor:color,
-      width:`${value}%`,
-    }}></div>
-  </div>
+const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
+  <div className="categoryItem">
+    <h5>{heading}</h5>
+    <div>
+      <div style={{
+        backgroundColor: color,
+        width: `${value}%`,
+      }}></div>
+    </div>
 
-  <span>{value}%</span>
-</div>
+    <span>{value}%</span>
+  </div>
 )
 
 export default dashboard;
